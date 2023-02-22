b58ce835d60cf98882dbed2dee2dae03bc3667db
Xing Ern <lxingern@gmail.com>
2023-02-22 15:34:58 +0800

    create endpoints to edit steps and active minutes data

--- backend/app.js
+++ backend/app.js
@@ -11,10 +11,26 @@ mongoose.connect(dbUrl, {
     useNewUrlParser: true
 })
 
+app.use(express.json());
+
 app.get('/', async (req, res) => {
     const todaysDate = getTodaysDate()
     const todaysData = await Activity.findOne({ date: todaysDate })
     res.status(200).send(todaysData.data)
+})
+
+app.patch('/activity/steps', async (req, res) => {
+    const todaysDate = getTodaysDate()
+    const updatedData = await Activity.findOneAndUpdate({ date: todaysDate }, { $set: { 'data.steps': +req.body.steps } }, { new: true })
+    await updatedData.save()
+    res.status(200).send(updatedData.data)
+})
+
+app.patch('/activity/activemins', async (req, res) => {
+    const todaysDate = getTodaysDate()
+    const updatedData = await Activity.findOneAndUpdate({ date: todaysDate }, { $set: { 'data.activeMinutes': +req.body.activeMinutes } }, { new: true })
+    await updatedData.save()
+    res.status(200).send(updatedData.data)
 })
 
 app.listen(port, () => {
